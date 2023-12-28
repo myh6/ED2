d19fbc37ae0f8f21f379c5810b3dc8bf2b6c53ec
myh <huang.minya@northeastern.edu>
2023-12-28 17:10:48 +0800

    Initial Commit

---
+++ EssentialDev@/EssentialDev_.h
@@ -0,0 +1,18 @@
+//
+//  EssentialDev_.h
+//  EssentialDev@
+//
+//  Created by Min-Yang Huang on 2023/12/28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialDev_.
+FOUNDATION_EXPORT double EssentialDev_VersionNumber;
+
+//! Project version string for EssentialDev_.
+FOUNDATION_EXPORT const unsigned char EssentialDev_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialDev_/PublicHeader.h>
+
+
