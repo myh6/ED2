795c0484c6998e8640f4484f6de1b74433423e12
myh <huang.minya@northeastern.edu>
2024-01-29 17:33:44 -0500

    [Persistence][3] Extract SUT creation to a factory method to protect tests from breaking changes

--- EssentialDevTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialDevTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -30,14 +30,12 @@ class FeedStore {
 class CacheFeedUseCaseTests: XCTestCase {
     
     func test_init_doesNotDeleteCacheUponCreation() {
-        let store = FeedStore()
-        _ = LocalFeedLoader(store: store)
+        let (_ , store) = makeSUT()
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
     }
     
     func test_save_reqeustCacheDeletion() {
-        let store = FeedStore()
-        let sut = LocalFeedLoader(store: store)
+        let (sut, store) = makeSUT()
         let items = [uniqueItem(), uniqueItem()]
         
         sut.save(items)
@@ -46,6 +44,14 @@ class CacheFeedUseCaseTests: XCTestCase {
     }
     
     //MARK: - Helper
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+    
     private func uniqueItem() -> FeedItem {
         return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
     }
