dce155b57d94edbab74c00bbedd1110e0169471d
myh <huang.minya@northeastern.edu>
2024-01-31 21:49:31 -0500

    [Persistence][4] Move LocalFeedLoader and FeedStore collaboratory to its own file in production

---
+++ EssentialDev/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentialDev
+//
+//  Created by Min-Yang Huang on 2024/1/31.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
---
+++ EssentialDev/Feed Cache/LocalFeedLoader.swift
@@ -0,0 +1,36 @@
+//
+//  LocalFeedLoader.swift
+//  EssentialDev
+//
+//  Created by Min-Yang Huang on 2024/1/31.
+//
+
+import Foundation
+
+public final class LocalFeedLoader {
+    private let store: FeedStore
+    private let currentDate: () -> Date
+    
+    public init(store: FeedStore, currentDate: @escaping () -> Date = Date.init) {
+        self.store = store
+        self.currentDate = currentDate
+    }
+    
+    
+    public func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
+        store.deleteCachedFeed { [weak self] error in
+            guard let self = self else { return }
+            if let cacheDeletionError = error {
+                completion(cacheDeletionError)
+            } else {
+                cache(items, with: completion)
+            }
+        }
+    }
+    private func cache(_ items: [FeedItem], with completion: @escaping (Error?) -> Void) {
+        store.insert(items, timestamp: currentDate()) { [weak self] error in
+            guard self != nil else { return }
+            completion(error)
+        }
+    }
+}
