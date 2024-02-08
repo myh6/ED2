1cdacc6341fa358ea9220a044557f804bc873023
myh <huang.minya@northeastern.edu>
2024-02-07 23:44:32 -0500

    [Persistence][8] Move FeedCachePolicy to its own file

---
+++ EssentialDev/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,22 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialDev
+//
+//  Created by Min-Yang Huang on 2024/2/7.
+//
+
+import Foundation
+
+final class FeedCachePolicy {
+    private init() {}
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    private static var maxCacheAgeInDays: Int {
+        return 7
+    }
+    
+    static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else { return false }
+        return date < maxCacheAge
+    }
+}
