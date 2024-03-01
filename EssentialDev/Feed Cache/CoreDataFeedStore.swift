921a89641b97c62cc9b69c2c6b19d9490c5a7345
myh <huang.minya@northeastern.edu>
2024-03-01 16:24:47 -0500

    [Persistence][15] Extract duplicate code into a reusable helper method

--- EssentialDev/Feed Cache/CoreDataFeedStore.swift
+++ EssentialDev/Feed Cache/CoreDataFeedStore.swift
@@ -18,8 +18,7 @@ public final class CoreDataFeedStore: FeedStore {
     }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        let context = self.context
-        context.perform {
+        perform { context in
             do {
                 try ManagedCache.find(in: context).map(context.delete).map(context.save)
                 completion(nil)
@@ -30,8 +29,7 @@ public final class CoreDataFeedStore: FeedStore {
     }
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        let context = self.context
-        context.perform {
+        perform { context in
             do {
                 let managedCache = try ManagedCache.newUniqueInstance(in: context)
                 managedCache.timestamp = timestamp
@@ -45,20 +43,22 @@ public final class CoreDataFeedStore: FeedStore {
     }
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        context.perform {
-            let context = self.context
-            context.perform {
-                do {
-                    if let cache = try ManagedCache.find(in: context) {
-                        completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
-                    } else {
-                        completion(.empty)
-                    }
-                } catch {
-                    completion(.failure(error))
+        perform { context in
+            do {
+                if let cache = try ManagedCache.find(in: context) {
+                    completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
+                } else {
+                    completion(.empty)
                 }
+            } catch {
+                completion(.failure(error))
             }
         }
+    }
+    
+    private func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
+        let context = self.context
+        context.perform { action(context) }
     }
     
 }
