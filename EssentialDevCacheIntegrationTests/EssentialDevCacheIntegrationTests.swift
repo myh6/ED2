c99b19cd5576935e36080e5edb8f1e9c3a3129ba
myh <huang.minya@northeastern.edu>
2024-03-04 19:55:55 -0500

    Add EssentialFeedCacheIntegrationTests target to separate the potentially slower cache integration tests from the fast unit/isolated tests

---
+++ EssentialDevCacheIntegrationTests/EssentialDevCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialDevCacheIntegrationTests.swift
+//  EssentialDevCacheIntegrationTests
+//
+//  Created by Min-Yang Huang on 2024/3/4.
+//
+
+import XCTest
+
+final class EssentialDevCacheIntegrationTests: XCTestCase {
+
+
+}
