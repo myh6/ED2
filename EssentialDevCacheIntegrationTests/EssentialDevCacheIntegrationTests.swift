cc0b9be52d1190f4124a9443ee2096be36686371
myh <huang.minya@northeastern.edu>
2024-03-04 20:06:54 -0500

    [Persistence][16]Clean up & undo all cache side-effects on setup and tearDown to a void sharing state between tests

--- EssentialDevCacheIntegrationTests/EssentialDevCacheIntegrationTests.swift
+++ EssentialDevCacheIntegrationTests/EssentialDevCacheIntegrationTests.swift
@@ -10,6 +10,18 @@ import EssentialDev
 
 final class EssentialDevCacheIntegrationTests: XCTestCase {
     
+    override func setUp() {
+        super.setUp()
+
+        setupEmptyStoreState()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+
+        undoStoreSideEffects()
+    }
+    
     func test_load_deliversNoItemsOnEmptyCache() {
         let sut = makeSUT()
         
@@ -45,5 +57,18 @@ final class EssentialDevCacheIntegrationTests: XCTestCase {
     private func cachesDirectory() -> URL {
         return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
     }
+    
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+    }
+
 
 }
