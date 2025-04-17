56cbff5af1e2e200735ffed4e897ccc615737402
myh <curryhuang3627@gmail.com>
2025-04-17 18:08:29 -0400

    Add EssentialDeviOS (prod and test) targets for iOS-specific components

---
+++ EssentialDeviOS/EssentialDeviOS.h
@@ -0,0 +1,18 @@
+//
+//  EssentialDeviOS.h
+//  EssentialDeviOS
+//
+//  Created by Min-Yang Huang on 4/17/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialDeviOS.
+FOUNDATION_EXPORT double EssentialDeviOSVersionNumber;
+
+//! Project version string for EssentialDeviOS.
+FOUNDATION_EXPORT const unsigned char EssentialDeviOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialDeviOS/PublicHeader.h>
+
+
