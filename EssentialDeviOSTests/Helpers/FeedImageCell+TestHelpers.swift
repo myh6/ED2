4b2923465b3fe827df860ba64e714c3876ae2261
myh <curryhuang3627@gmail.com>
2025-04-20 15:48:59 -0400

    Extract FeedImageCell test helpers to a separate file

--- EssentialDeviOSTests/Helpers/FeedImageCell+TestHelpers.swift
+++ EssentialDeviOSTests/Helpers/FeedImageCell+TestHelpers.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import EssentialDeviOS
+
+extension FeedImageCell {
+    var isShowingLocation: Bool {
+        return !locationContainer.isHidden
+    }
+    
+    var locationText: String? {
+        return locationLabel.text
+    }
+    
+    var descriptionText: String? {
+        return descriptionLabel.text
+    }
+    
+    var isShowingImageLoadingIndicator: Bool {
+        return feedImageContainer.isShimmering
+    }
+    
+    var renderedImage: Data? {
+        return feedImageView.image?.pngData()
+    }
+    
+    var isShowingRetryAction: Bool {
+        return !feedImageRetryButton.isHidden
+    }
+    
+    func simulateRetryAction() {
+        feedImageRetryButton.simulateTap()
+    }
+}
