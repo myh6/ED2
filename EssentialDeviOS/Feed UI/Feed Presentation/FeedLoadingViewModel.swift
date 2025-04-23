1895e623b545d0dc0f0bc93a4b406a53ae0fc344
myh <curryhuang3627@gmail.com>
2025-04-22 23:33:35 -0400

    Move ViewModels to separate files

---
+++ EssentialDeviOS/Feed UI/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialDeviOS
+//
+//  Created by Min-Yang Huang on 4/22/25.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialDeviOS/Feed UI/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentialDeviOS
+//
+//  Created by Min-Yang Huang on 4/22/25.
+//
+
+import Foundation
+import EssentialDev
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
