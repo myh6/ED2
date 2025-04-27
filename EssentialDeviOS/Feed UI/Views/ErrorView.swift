074752d6cde3d8ca1cf581fd66b42d467200dc4c
Min Yang Huang <curryhuang3627@gmail.com>
2025-04-27 18:17:50 -0400

    Move `ErrorView` to separate file

---
+++ EssentialDeviOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialDeviOS
+//
+//  Created by Min-Yang Huang on 4/27/25.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
