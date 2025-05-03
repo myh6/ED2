8624b44ed94793690778583a5fee89ada31d57b2
Min Yang Huang <curryhuang3627@gmail.com>
2025-05-03 17:18:57 -0400

    Extract test helpers into a shared scope to remove duplication

--- EssentialDevApp/EssentialDevAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ EssentialDevApp/EssentialDevAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -47,31 +47,27 @@ final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
     
     private func expect(_ sut: FeedLoader, toCompleteWith expectedResult: FeedLoader.Result, file: StaticString = #file, line: UInt = #line) {
         let exp = expectation(description: "Wait for load completion")
-
+        
         sut.load { receivedResult in
             switch (receivedResult, expectedResult) {
             case let (.success(receivedFeed), .success(expectedFeed)):
                 XCTAssertEqual(receivedFeed, expectedFeed, file: file, line: line)
-
+                
             case (.failure, .failure):
                 break
-
+                
             default:
                 XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
             }
-
+            
             exp.fulfill()
         }
-
+        
         wait(for: [exp], timeout: 1.0)
-    }
-
-    private func anyNSError() -> NSError {
-        return NSError(domain: "any error", code: 0)
     }
     
     private func uniqueFeed() -> [FeedImage] {
-        return [FeedImage(id: UUID(), description: "any", location: "any", url: URL(string: "http://any-url.com")!)]
+        return [FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())]
     }
 
     private class LoaderStub: FeedLoader {
