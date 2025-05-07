f6074fdf8dabb61284e770ad29dc48d9a55acec5
Min Yang Huang <curryhuang3627@gmail.com>
2025-05-06 20:31:32 -0400

    Remove unused debugging code that was previously used during UI tests

--- EssentialDevApp/EssentialDevApp/AppDelegate.swift
+++ EssentialDevApp/EssentialDevApp/AppDelegate.swift
@@ -20,13 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        let configuration = UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-        
-        #if DEBUG
-        configuration.delegateClass = DebuggingSceneDelegate.self
-        #endif
-        
-        return configuration
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
