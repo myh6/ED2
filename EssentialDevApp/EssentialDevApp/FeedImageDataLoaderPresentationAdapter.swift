4f98233683d9022993b995efba28792041ac3f6d
Min Yang Huang <curryhuang3627@gmail.com>
2025-05-10 16:47:51 -0400

    Replace FeedImageDataLoader composition with Combine operators

--- EssentialDevApp/EssentialDevApp/FeedImageDataLoaderPresentationAdapter.swift
+++ EssentialDevApp/EssentialDevApp/FeedImageDataLoaderPresentationAdapter.swift
@@ -5,17 +5,19 @@
 //  Created by Min-Yang Huang on 4/26/25.
 //
 
+import Foundation
+import Combine
 import EssentialDev
 import EssentialDeviOS
 
 final class FeedImageDataLoaderPresentationAdapter<View: FeedImageView, Image>: FeedImageCellControllerDelegate where View.Image == Image {
     private let model: FeedImage
-    private let imageLoader: FeedImageDataLoader
-    private var task: FeedImageDataLoaderTask?
+    private let imageLoader: (URL) -> FeedImageDataLoader.Publisher
+    private var cancellable: Cancellable?
     
     var presenter: FeedImagePresenter<View, Image>?
     
-    init(model: FeedImage, imageLoader: FeedImageDataLoader) {
+    init(model: FeedImage, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher) {
         self.model = model
         self.imageLoader = imageLoader
     }
@@ -24,17 +26,19 @@ final class FeedImageDataLoaderPresentationAdapter<View: FeedImageView, Image>: 
         presenter?.didStartLoadingImageData(for: model)
         
         let model = self.model
-        task = imageLoader.loadImageData(from: model.url) { [weak self] result in
-            switch result {
-            case let .success(data):
-                self?.presenter?.didFinishLoadingImageData(with: data, for: model)
+        
+        cancellable = imageLoader(model.url).sink { [weak self] completion in
+            switch completion {
+            case .finished: break
             case let .failure(error):
                 self?.presenter?.didFinishLoadingImageData(with: error, for: model)
             }
+        } receiveValue: { [weak self] data in
+            self?.presenter?.didFinishLoadingImageData(with: data, for: model)
         }
     }
     
     func didCancelImageRequest() {
-        task?.cancel()
+        cancellable?.cancel()
     }
 }
