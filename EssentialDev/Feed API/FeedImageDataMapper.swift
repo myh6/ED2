e302051b70e4936a83f11b8dcc7bd4b800bebe92
Min Yang Huang <curryhuang3627@gmail.com>
2025-05-12 00:02:42 -0400

    Test FeedImageDataMapper in isolation

---
+++ EssentialDev/Feed API/FeedImageDataMapper.swift
@@ -0,0 +1,22 @@
+//
+//  FeedImageDataMapper.swift
+//  EssentialDev
+//
+//  Created by Min-Yang Huang on 5/11/25.
+//
+
+import Foundation
+
+public final class FeedImageDataMapper {
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+    
+    public static func map(_ data: Data, from response: HTTPURLResponse) throws -> Data {
+        guard response.isOK, !data.isEmpty else {
+            throw Error.invalidData
+        }
+        
+        return data
+    }
+}
