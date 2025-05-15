4ec0ab4ce5feeb23f8dcdd0e83bd3fc0e77739d8
Min Yang Huang <curryhuang3627@gmail.com>
2025-05-14 21:24:03 -0400

    Rename and move ResourceLoadingView and VIewModel to Shared module

---
+++ EssentialDev/Shared Presentation/ResourceLoadingView.swift
@@ -0,0 +1,12 @@
+//
+//  ResourceLoadingView.swift
+//  EssentialDev
+//
+//  Created by Min-Yang Huang on 5/14/25.
+//
+
+import Foundation
+
+public protocol ResourceLoadingView {
+    func display(_ viewModel: ResourceLoadingViewModel)
+}
